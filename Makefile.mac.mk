c96aa9d16e03008581fb7cce28c9169c5a36b9a3
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-04-13 15:35:01 +0200

    Rely on git to know when to trigger make in a given module

--- Makefile.mac.mk
+++ Makefile.mac.mk
@@ -13,89 +13,69 @@ distclean:
 clean:
 	rm -f build/*.rc
 	rm -f build/*.site
-	rm -f build/*.stamp
+	rm -f build/*-stamp
 	rm -rf build/frida-mac32
 	rm -rf build/frida-mac64
 	rm -rf build/frida-ios
 	rm -rf build/tmp-mac32
 	rm -rf build/tmp-mac64
 	rm -rf build/tmp-ios
+	cd udis86 && git clean -xfd
+	cd frida-gum && git clean -xfd
 
 
-udis86: udis86-mac32 udis86-mac64
+udis86: \
+	udis86-update-repo-stamp \
+	build/frida-mac32/lib/pkgconfig/udis86.pc \
+	build/frida-mac64/lib/pkgconfig/udis86.pc
 
-udis86-mac32: env-mac32 udis86-autogen udis86-mac32-makefile
-	source build/frida-env-mac32.rc && cd $(mac32)/udis86 && make install
-
-udis86-mac64: env-mac64 udis86-autogen udis86-mac64-makefile
-	source build/frida-env-mac64.rc && cd $(mac64)/udis86 && make install
-
-udis86-autogen: build/udis86-autogen.stamp
-build/udis86-autogen.stamp: env-mac64
+udis86/configure: env-mac64 udis86/configure.ac
 	source build/frida-env-mac64.rc && cd udis86 && ./autogen.sh
-	@touch $@
 
-udis86-mac32-makefile: $(mac32)/udis86-makefile.stamp
-$(mac32)/udis86-makefile.stamp: udis86/configure
-	mkdir -p $(mac32)/udis86
-	source build/frida-env-mac32.rc && cd $(mac32)/udis86 && $(udis86)/configure
-	@touch $@
+build/tmp-%/udis86/Makefile: udis86/configure
+	mkdir -p $(@D)
+	source build/frida-env-$*.rc && cd $(@D) && $(udis86)/configure
 
-udis86-mac64-makefile: $(mac64)/udis86-makefile.stamp
-$(mac64)/udis86-makefile.stamp: udis86/configure
-	mkdir -p $(mac64)/udis86
-	source build/frida-env-mac64.rc && cd $(mac64)/udis86 && $(udis86)/configure
-	@touch $@
+build/frida-%/lib/pkgconfig/udis86.pc: env-% udis86/configure build/tmp-%/udis86/Makefile build/udis86-repo-stamp
+	source build/frida-env-$*.rc && cd build/tmp-$*/udis86 && make install
+	touch $@
 
 
-frida-gum: frida-gum-mac32 frida-gum-mac64
-
-frida-gum-mac32: env-mac32 frida-gum-autogen frida-gum-mac32-makefile
-	source build/frida-env-mac32.rc && cd $(mac32)/frida-gum && make install
-
-frida-gum-mac64: env-mac64 frida-gum-autogen frida-gum-mac64-makefile
-	source build/frida-env-mac64.rc && cd $(mac64)/frida-gum && make install
-
-frida-gum-autogen: build/frida-gum-autogen.stamp env-mac64
-build/frida-gum-autogen.stamp: frida-gum/configure.ac
-	source build/frida-env-mac64.rc && cd frida-gum && ./autogen.sh
-	@touch $@
-
-frida-gum-mac32-makefile: $(mac32)/frida-gum-makefile.stamp
-$(mac32)/frida-gum-makefile.stamp: udis86-mac32 frida-gum/configure
-	mkdir -p $(mac32)/frida-gum
-	source build/frida-env-mac32.rc && cd $(mac32)/frida-gum && $(frida_gum)/configure
-	@touch $@
-
-frida-gum-mac64-makefile: $(mac64)/frida-gum-makefile.stamp
-$(mac64)/frida-gum-makefile.stamp: udis86-mac64 frida-gum/configure
-	mkdir -p $(mac64)/frida-gum
-	source build/frida-env-mac64.rc && cd $(mac64)/frida-gum && $(frida_gum)/configure
-	@touch $@
+udis86-update-repo-stamp:
+	@cd udis86 && git log -1 --format=%H > ../build/udis86-repo-stamp.tmp && git status >> ../build/udis86-repo-stamp.tmp
+	@if [ -f build/udis86-repo-stamp ]; then \
+		if cmp -s build/udis86-repo-stamp build/udis86-repo-stamp.tmp; then \
+			rm build/udis86-repo-stamp.tmp; \
+		else \
+			mv build/udis86-repo-stamp.tmp build/udis86-repo-stamp; \
+		fi \
+	else \
+		mv build/udis86-repo-stamp.tmp build/udis86-repo-stamp; \
+	fi
 
 
-env-mac32: build/frida-env-mac32.stamp
-build/frida-env-mac32.stamp:
+env-mac32: build/frida-env-mac32-stamp
+build/frida-env-mac32-stamp:
 	FRIDA_TARGET=mac32 ./setup-env.sh
 	mkdir -p $(mac32)
-	touch build/frida-env-mac32.stamp
+	touch build/frida-env-mac32-stamp
 
-env-mac64: build/frida-env-mac64.stamp
-build/frida-env-mac64.stamp:
+env-mac64: build/frida-env-mac64-stamp
+build/frida-env-mac64-stamp:
 	FRIDA_TARGET=mac64 ./setup-env.sh
 	mkdir -p $(mac64)
-	touch build/frida-env-mac64.stamp
+	touch build/frida-env-mac64-stamp
 
-env-ios: build/frida-env-ios.stamp
-build/frida-env-ios.stamp:
+env-ios: build/frida-env-ios-stamp
+build/frida-env-ios-stamp:
 	FRIDA_TARGET=ios ./setup-env.sh
 	mkdir -p $(ios)
-	touch build/frida-env-ios.stamp
+	touch build/frida-env-ios-stamp
 
 
 .PHONY: \
-	distclean clean
+	distclean clean \
+	udis86 udis86-update-repo-stamp
 .INTERMEDIATE: \
-	udis86 udis86-mac32 udis86-mac64 udis86-autogen udis86-mac32-makefile udis86-mac64-makefile \
-	frida-gum frida-gum-mac32 frida-gum-mac64 frida-gum-autogen frida-gum-mac32-makefile frida-gum-mac64-makefile \
 	env-mac32 env-mac64 env-ios
+.SECONDARY:
