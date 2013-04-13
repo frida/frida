94ea5123d08ff86a2d724f33d4f8af7b3f2710f6
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-04-13 18:19:04 +0200

    Improve the Mac makefile to solve remaining issues

--- Makefile.mac.mk
+++ Makefile.mac.mk
@@ -1,7 +1,4 @@
 modules = udis86 frida-gum
-mac32 = build/tmp-mac32
-mac64 = build/tmp-mac64
-ios = build/tmp-ios
 
 
 all: udis86 frida-gum
@@ -24,37 +21,40 @@ clean:
 
 
 udis86: \
-	udis86-update-repo-stamp \
 	build/frida-mac32/lib/pkgconfig/udis86.pc \
 	build/frida-mac64/lib/pkgconfig/udis86.pc
 
-udis86/configure: env-mac64 udis86/configure.ac
+udis86/configure: build/frida-env-mac64.rc udis86/configure.ac
 	source build/frida-env-mac64.rc && cd udis86 && ./autogen.sh
 
-build/tmp-%/udis86/Makefile: udis86/configure
+build/tmp-%/udis86/Makefile: build/frida-env-%.rc udis86/configure
 	mkdir -p $(@D)
 	source build/frida-env-$*.rc && cd $(@D) && ../../../udis86/configure
 
-build/frida-%/lib/pkgconfig/udis86.pc: env-% udis86/configure build/tmp-%/udis86/Makefile build/udis86-repo-stamp
+build/frida-%/lib/pkgconfig/udis86.pc: build/tmp-%/udis86/Makefile build/udis86-repo-stamp
 	source build/frida-env-$*.rc && cd build/tmp-$*/udis86 && make install
 	touch $@
 
 
 frida-gum: \
-	frida-gum-update-repo-stamp \
+	udis86 \
 	build/frida-mac32/lib/pkgconfig/frida-gum-1.0.pc \
 	build/frida-mac64/lib/pkgconfig/frida-gum-1.0.pc
 
-frida-gum/configure: env-mac64 frida-gum/configure.ac
+frida-gum/configure: build/frida-env-mac64.rc frida-gum/configure.ac
 	source build/frida-env-mac64.rc && cd frida-gum && ./autogen.sh
 
-build/tmp-%/frida-gum/Makefile: frida-gum/configure build/frida-%/lib/pkgconfig/udis86.pc
+build/tmp-%/frida-gum/Makefile: build/frida-env-%.rc frida-gum/configure
 	mkdir -p $(@D)
 	source build/frida-env-$*.rc && cd $(@D) && ../../../frida-gum/configure
 
-build/frida-%/lib/pkgconfig/frida-gum-1.0.pc: env-% frida-gum/configure build/tmp-%/frida-gum/Makefile build/frida-gum-repo-stamp
+build/frida-%/lib/pkgconfig/frida-gum-1.0.pc: build/tmp-%/frida-gum/Makefile build/udis86-repo-stamp build/frida-gum-repo-stamp
 	source build/frida-env-$*.rc && cd build/tmp-$*/frida-gum && make install
 	touch $@
+
+
+build/frida-env-%.rc:
+	FRIDA_TARGET=$* ./setup-env.sh
 
 
 define make-update-repo-stamp
@@ -71,31 +71,12 @@ $1-update-repo-stamp:
 	fi
 endef
 $(foreach m,$(modules),$(eval $(call make-update-repo-stamp,$m)))
-
-
-env-mac32: build/frida-env-mac32-stamp
-build/frida-env-mac32-stamp:
-	FRIDA_TARGET=mac32 ./setup-env.sh
-	mkdir -p $(mac32)
-	touch build/frida-env-mac32-stamp
-
-env-mac64: build/frida-env-mac64-stamp
-build/frida-env-mac64-stamp:
-	FRIDA_TARGET=mac64 ./setup-env.sh
-	mkdir -p $(mac64)
-	touch build/frida-env-mac64-stamp
-
-env-ios: build/frida-env-ios-stamp
-build/frida-env-ios-stamp:
-	FRIDA_TARGET=ios ./setup-env.sh
-	mkdir -p $(ios)
-	touch build/frida-env-ios-stamp
+module-stamps: $(foreach m,$(modules),$m-update-repo-stamp)
+-include module-stamps
 
 
 .PHONY: \
-	distclean clean \
+	distclean clean module-stamps \
 	udis86 udis86-update-repo-stamp \
 	frida-gum frida-gum-update-repo-stamp
-.INTERMEDIATE: \
-	env-mac32 env-mac64 env-ios
 .SECONDARY:
