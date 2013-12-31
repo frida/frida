a376b19bc29cefd4c6d6244e03bbd46e10be570b
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2013-12-31 11:30:58 +0100

    Add iOS deb packaging and upload to the release script

--- release.py
+++ release.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     import subprocess
 
     build_dir = os.path.dirname(os.path.realpath(__file__))
+    toolchain_dir = os.path.join(build_dir, "build", "toolchain")
+    frida_core_dir = os.path.join(build_dir, "frida-core")
     frida_python_dir = os.path.join(build_dir, "frida-python")
 
     raw_version = subprocess.check_output(["git", "describe", "--tags", "--always", "--long"], cwd=build_dir).strip().replace("-", ".")
@@ -20,6 +22,18 @@ if __name__ == '__main__':
         env.update(os.environ)
         subprocess.call([interpreter, "setup.py", "bdist_egg", "upload"], cwd=os.path.join(frida_python_dir, "src"), env=env)
 
+    def upload_ios_deb(server):
+        env = {
+            'FRIDA_VERSION': version,
+            'FRIDA_TOOLCHAIN': toolchain_dir
+        }
+        env.update(os.environ)
+        deb = os.path.join(build_dir, "frida_%s_iphoneos-arm.deb" % version)
+        subprocess.call([os.path.join(frida_core_dir, "tools", "package-server.sh"), server, deb], env=env)
+        subprocess.call(["scp", deb, "buildmaster@ospy.org:/home/buildmaster/public_html/debs/"])
+        subprocess.call(["ssh", "buildmaster@ospy.org", "/home/buildmaster/cydia/sync-repo"])
+        os.unlink(deb)
+
     if int(nano) == 0:
         system = platform.system()
         if system == 'Windows':
@@ -32,6 +46,7 @@ if __name__ == '__main__':
                 os.path.join(build_dir, "build", "frida-mac-universal", "lib", "python2.6", "site-packages", "_frida.so"))
             upload_to_pypi("python2.7",
                 os.path.join(build_dir, "build", "frida-mac-universal", "lib", "python2.7", "site-packages", "_frida.so"))
+            upload_ios_deb(os.path.join(build_dir, "build", "frida-ios", "bin", "frida-server"))
         elif system == 'Linux':
             upload_to_pypi("python2.7",
                 os.path.join(build_dir, "build", "frida-linux-x86_64-stripped", "lib", "python2.7", "site-packages", "_frida.so"))
