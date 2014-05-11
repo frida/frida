64a6d05982960b711684a07347030c37160b847a
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-05-11 13:22:53 +0200

    Improve SDK build targets to avoid interference with other environments

--- Makefile.sdk.mk
+++ Makefile.sdk.mk
@@ -31,29 +31,29 @@ host_platform_arch := $(host_platform)-$(host_arch)
 
 
 ifeq ($(host_platform), linux)
-	bfd := build/frida-%/lib/libbfd.a
+	bfd := build/fs-%/lib/libbfd.a
 endif
 ifeq ($(host_platform), android)
-	iconv := build/frida-%/lib/libiconv.a
-	bfd := build/frida-%/lib/libbfd.a
+	iconv := build/fs-%/lib/libiconv.a
+	bfd := build/fs-%/lib/libbfd.a
 endif
 
 
-all: build/tmp-$(host_platform_arch)/sdk-package-stamp
+all: build/tmp-$(host_platform_arch)/.sdk-package-stamp
 
 
-build/tmp-%/sdk-package-stamp: \
+build/tmp-%/.sdk-package-stamp: \
 		$(iconv) \
 		$(bfd) \
-		build/frida-%/lib/pkgconfig/libffi.pc \
-		build/frida-%/lib/pkgconfig/glib-2.0.pc \
-		build/frida-%/lib/pkgconfig/gee-1.0.pc \
-		build/frida-%/lib/pkgconfig/json-glib-1.0.pc \
-		build/frida-%/lib/pkgconfig/v8.pc
+		build/fs-%/lib/pkgconfig/libffi.pc \
+		build/fs-%/lib/pkgconfig/glib-2.0.pc \
+		build/fs-%/lib/pkgconfig/gee-1.0.pc \
+		build/fs-%/lib/pkgconfig/json-glib-1.0.pc \
+		build/fs-%/lib/pkgconfig/v8.pc
 
 
-build/binutils-stamp: build/.clean-sdk-stamp
-	$(RM) -rf binutils
+build/.binutils-stamp:
+	$(RM) -r binutils
 	mkdir binutils
 	cd binutils \
 		&& $(download) http://gnuftp.uib.no/binutils/binutils-2.24.tar.bz2 | tar -xj --strip-components 1 \
@@ -61,23 +61,23 @@ build/binutils-stamp: build/.clean-sdk-stamp
 	@mkdir -p $(@D)
 	@touch $@
 
-build/tmp-%/binutils/libiberty/Makefile: build/frida-env-%.rc build/binutils-stamp
-	$(RM) -rf $(@D)
+build/tmp-%/binutils/libiberty/Makefile: build/fs-env-%.rc build/.binutils-stamp
+	$(RM) -r $(@D)
 	mkdir -p $(@D)
 	. $< && cd $(@D) && ../../../../binutils/libiberty/configure
 
-build/tmp-%/binutils/bfd/Makefile: build/frida-env-%.rc build/binutils-stamp
-	$(RM) -rf $(@D)
+build/tmp-%/binutils/bfd/Makefile: build/fs-env-%.rc build/.binutils-stamp
+	$(RM) -r $(@D)
 	mkdir -p $(@D)
 	. $< && cd $(@D) && ../../../../binutils/bfd/configure
 
-build/frida-%/lib/libbfd.a: \
-		build/frida-env-%.rc \
-		build/frida-%/include/bfd.h \
+build/fs-%/lib/libbfd.a: \
+		build/fs-env-%.rc \
+		build/fs-%/include/bfd.h \
 		build/tmp-%/binutils/libiberty/libiberty.a \
 		build/tmp-%/binutils/bfd/libbfd.a
 	mkdir -p $(@D)
-	rm -rf build/tmp-$*/binutils/tmp
+	$(RM) -r build/tmp-$*/binutils/tmp
 	mkdir build/tmp-$*/binutils/tmp
 	. $< \
 		&& cd build/tmp-$*/binutils/tmp \
@@ -87,40 +87,40 @@ build/frida-%/lib/libbfd.a: \
 		&& $$RANLIB libbfd-full.a \
 		&& install -m 644 libbfd-full.a ../../../../$@
 
-build/frida-%/include/bfd.h: build/frida-env-%.rc build/tmp-%/binutils/bfd/Makefile
+build/fs-%/include/bfd.h: build/fs-env-%.rc build/tmp-%/binutils/bfd/Makefile
 	. $< && make -C build/tmp-$*/binutils/bfd $(MAKE_J) install-bfdincludeHEADERS
 
-build/tmp-%/binutils/libiberty/libiberty.a: build/frida-env-%.rc build/tmp-%/binutils/libiberty/Makefile
+build/tmp-%/binutils/libiberty/libiberty.a: build/fs-env-%.rc build/tmp-%/binutils/libiberty/Makefile
 	. $< && make -C $(@D) $(MAKE_J)
 
-build/tmp-%/binutils/bfd/libbfd.a: build/frida-env-%.rc build/tmp-%/binutils/bfd/Makefile
+build/tmp-%/binutils/bfd/libbfd.a: build/fs-env-%.rc build/tmp-%/binutils/bfd/Makefile
 	. $< && make -C $(@D) $(MAKE_J)
 
 
 define make-plain-module-rules
-build/$1-stamp: build/.clean-sdk-stamp
-	$(RM) -rf $1
+build/.$1-stamp:
+	$(RM) -r $1
 	git clone $(REPO_BASE_URL)/$1$(REPO_SUFFIX)
 	@mkdir -p $$(@D)
 	@touch $$@
 
-$1/configure: build/frida-env-$(build_platform_arch).rc build/$1-stamp
+$1/configure: build/fs-env-$(build_platform_arch).rc build/.$1-stamp
 	. $$< && cd $$(@D) && NOCONFIGURE=1 ./autogen.sh
 
-build/tmp-%/$1/Makefile: build/frida-env-%.rc $1/configure
-	$(RM) -rf $$(@D)
+build/tmp-%/$1/Makefile: build/fs-env-%.rc $1/configure
+	$(RM) -r $$(@D)
 	mkdir -p $$(@D)
 	. $$< && cd $$(@D) && ../../../$1/configure
 
-build/frida-%/lib/pkgconfig/$2.pc: build/frida-env-%.rc build/tmp-%/$1/Makefile $3
+build/fs-%/lib/pkgconfig/$2.pc: build/fs-env-%.rc build/tmp-%/$1/Makefile $3
 	. $$< && make -C build/tmp-$$*/$1 $(MAKE_J) install GLIB_GENMARSHAL=glib-genmarshal GLIB_MKENUMS=glib-mkenums
 	@touch $$@
 endef
 
 $(eval $(call make-plain-module-rules,libffi,libffi,))
 $(eval $(call make-plain-module-rules,glib,glib-2.0,))
-$(eval $(call make-plain-module-rules,libgee,gee-1.0,build/frida-%/lib/pkgconfig/glib-2.0.pc))
-$(eval $(call make-plain-module-rules,json-glib,json-glib-1.0,build/frida-%/lib/pkgconfig/glib-2.0.pc))
+$(eval $(call make-plain-module-rules,libgee,gee-1.0,build/fs-%/lib/pkgconfig/glib-2.0.pc))
+$(eval $(call make-plain-module-rules,json-glib,json-glib-1.0,build/fs-%/lib/pkgconfig/glib-2.0.pc))
 
 
 ifeq ($(host_arch), i386)
@@ -181,19 +181,19 @@ else
 		LINK="$$CXX"
 endif
 
-build/v8-stamp: build/.clean-sdk-stamp
-	$(RM) -rf v8
+build/.v8-stamp:
+	$(RM) -r v8
 	git clone $(REPO_BASE_URL)/v8$(REPO_SUFFIX)
 	@mkdir -p $(@D)
 	@touch $@
 
-build/tmp-%/v8-stamp: build/v8-stamp
+build/tmp-%/.v8-stamp: build/.v8-stamp
 	# Poor-man's substitute for out-of-tree builds
 	@mkdir -p $(@D)
 	git clone --depth 1 v8 $(@D)/v8
 	@touch $@
 
-build/tmp-%/v8/out/$(v8_target)/libv8_base.$(v8_arch).a: build/frida-env-%.rc build/tmp-%/v8-stamp
+build/tmp-%/v8/out/$(v8_target)/libv8_base.$(v8_arch).a: build/fs-env-%.rc build/tmp-%/.v8-stamp
 	. $< \
 		&& cd build/tmp-$*/v8 \
 		&& PATH="/usr/bin:/bin:/usr/sbin:/sbin" \
@@ -201,12 +201,12 @@ build/tmp-%/v8/out/$(v8_target)/libv8_base.$(v8_arch).a: build/frida-env-%.rc bu
 			make $(v8_target) GYPFLAGS="$(v8_flags)" V=1
 	@touch $@
 
-build/frida-%/lib/pkgconfig/v8.pc: build/tmp-%/v8/out/$(v8_target)/libv8_base.$(v8_arch).a
-	install -d build/frida-$*/include
-	install -m 644 v8/include/* build/frida-$*/include
-	install -d build/frida-$*/lib
-	install -m 644 build/tmp-$*/v8/out/$(v8_target)/libv8_base.$(v8_arch).a build/frida-$*/lib
-	install -m 644 build/tmp-$*/v8/out/$(v8_target)/libv8_snapshot.a build/frida-$*/lib
+build/fs-%/lib/pkgconfig/v8.pc: build/tmp-%/v8/out/$(v8_target)/libv8_base.$(v8_arch).a
+	install -d build/fs-$*/include
+	install -m 644 v8/include/* build/fs-$*/include
+	install -d build/fs-$*/lib
+	install -m 644 build/tmp-$*/v8/out/$(v8_target)/libv8_base.$(v8_arch).a build/fs-$*/lib
+	install -m 644 build/tmp-$*/v8/out/$(v8_target)/libv8_snapshot.a build/fs-$*/lib
 	install -d $(@D)
 	echo "prefix=\$${frida_sdk_prefix}" > $@.tmp
 	echo "exec_prefix=\$${prefix}" >> $@.tmp
@@ -221,14 +221,14 @@ build/frida-%/lib/pkgconfig/v8.pc: build/tmp-%/v8/out/$(v8_target)/libv8_base.$(
 	mv $@.tmp $@
 
 
-build/.clean-sdk-stamp:
-	rm -rf build
-	mkdir -p build/sdk-$(host_platform_arch)/share/aclocal
-	touch build/sdk-$(host_platform_arch)/.stamp
+build/.fs-sdk-stamp:
+	$(RM) -r build/fs-sdk-$(host_platform_arch)
+	mkdir -p build/fs-sdk-$(host_platform_arch)/share/aclocal
+	touch build/fs-sdk-$(host_platform_arch)/.stamp
 	touch $@
 
-build/frida-env-%.rc: build/.clean-sdk-stamp
-	FRIDA_HOST=$* ./releng/setup-env.sh
+build/fs-env-%.rc: build/.fs-sdk-stamp
+	FRIDA_HOST=$* FRIDA_ENV_NAME=$(env_name) ./releng/setup-env.sh
 
 
 .PHONY: all iconv bfd
