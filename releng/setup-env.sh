6c80c5cea2d4c6adeebf1310be1f25e17de7540d
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-07-20 22:35:23 +0200

    Bump toolchain and SDK versions

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -43,8 +43,8 @@ fi
 
 prompt_color=33
 
-toolchain_version=20140511
-sdk_version=20140511
+toolchain_version=20140720
+sdk_version=20140720
 
 if [ -n "$FRIDA_ENV_NAME" ]; then
   frida_env_name_prefix=${FRIDA_ENV_NAME}-
