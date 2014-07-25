f08587f845df145f902c2ac86d1cb6b274cdaa86
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-07-26 00:38:18 +0200

    Bump toolchain and SDK versions
    The only changes are ARM64-specific libffi bugfixes, but might as well
    harmonize the versions since they're essentially the same.

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -43,8 +43,8 @@ fi
 
 prompt_color=33
 
-toolchain_version=20140721
-sdk_version=20140720
+toolchain_version=20140726
+sdk_version=20140726
 
 if [ -n "$FRIDA_ENV_NAME" ]; then
   frida_env_name_prefix=${FRIDA_ENV_NAME}-
