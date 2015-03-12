3a685666633d8832d3129cf9a821bbd24f88e4c9
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2015-03-12 19:42:11 +0000

    Fix the Linux check targets

--- Makefile.linux.mk
+++ Makefile.linux.mk
@@ -98,10 +98,10 @@ build/frida-%/lib/pkgconfig/frida-gum-1.0.pc: build/tmp-%/frida-gum/Makefile bui
 	. build/frida-env-$*.rc && make -C build/tmp-$*/frida-gum install
 	@touch -c $@
 
-check-gum-32: check-gum-linux-i386 ##@gum Run tests for i386
-check-gum-64: check-gum-linux-x86_64 ##@gum Run tests for x86-64
-check-gum-linux-%: build/frida-linux-%/lib/pkgconfig/frida-gum-1.0.pc
-	build/tmp-linux-$*/frida-gum/tests/gum-tests
+check-gum-32: build/frida-linux-i386/lib/pkgconfig/frida-gum-1.0.pc ##@gum Run tests for i386
+	build/tmp-linux-i386/frida-gum/tests/gum-tests
+check-gum-64: build/frida-linux-x86_64/lib/pkgconfig/frida-gum-1.0.pc ##@gum Run tests for x86-64
+	build/tmp-linux-x86_64/frida-gum/tests/gum-tests
 
 
 core-32: build/frida-linux-i386/lib/pkgconfig/frida-core-1.0.pc ##@core Build for i386
@@ -146,9 +146,9 @@ build/tmp-%/frida-core/tests/frida-tests: build/frida-%/lib/pkgconfig/frida-core
 	. build/frida-env-$*.rc && make -C build/tmp-$*/frida-core/tests
 	@touch -c $@
 
-check-core-32: check-core-linux-i386 ##@core Run tests for i386
-check-core-64: check-core-linux-x86_64 ##@core Run tests for x86-64
-check-core-linux-%: build/tmp-linux-%/frida-core/tests/frida-tests
+check-core-32: build/tmp-linux-i386/frida-core/tests/frida-tests ##@core Run tests for i386
+	$<
+check-core-64: build/tmp-linux-x86_64/frida-core/tests/frida-tests ##@core Run tests for x86-64
 	$<
 
 
@@ -160,13 +160,11 @@ build/frida-android-i386-stripped/bin/frida-server: build/frida-android-i386/bin
 	cp $< $@.tmp
 	. build/frida-env-android-i386.rc && $$STRIP --strip-all $@.tmp
 	mv $@.tmp $@
-
 build/frida-android-arm-stripped/bin/frida-server: build/frida-android-arm/bin/frida-server
 	mkdir -p $(@D)
 	cp $< $@.tmp
 	. build/frida-env-android-arm.rc && $$STRIP --strip-all $@.tmp
 	mv $@.tmp $@
-
 build/frida-%/bin/frida-server: build/frida-%/lib/pkgconfig/frida-core-1.0.pc
 	@$(call ensure_relink,frida-core/server/server.c,build/tmp-$*/frida-core/server/frida_server-server.o)
 	. build/frida-env-$*.rc && make -C build/tmp-$*/frida-core/server install
@@ -200,11 +198,12 @@ build/frida-%-stripped/lib/$(PYTHON_NAME)/site-packages/_frida.so: build/tmp-%/f
 	cp build/tmp-$*/frida-$(PYTHON_NAME)/src/.libs/_frida.so $@
 	strip --strip-all $@
 
-check-python-32: check-python-i386 ##@bindings Test Python bindings for i386
-check-python-64: check-python-x86_64 ##@bindings Test Python bindings for x86-64
-
-check-python-%: build/frida-%-stripped/lib/$(PYTHON_NAME)/site-packages/frida build/frida-%-stripped/lib/$(PYTHON_NAME)/site-packages/_frida.so
-	export PYTHONPATH="$(shell pwd)/build/frida-linux-$*-stripped/lib/$(PYTHON_NAME)/site-packages" \
+check-python-32: build/frida-linux-i386-stripped/lib/$(PYTHON_NAME)/site-packages/frida build/frida-linux-i386-stripped/lib/$(PYTHON_NAME)/site-packages/_frida.so ##@bindings Test Python bindings for i386
+	export PYTHONPATH="$(shell pwd)/build/frida-linux-i386-stripped/lib/$(PYTHON_NAME)/site-packages" \
+		&& cd frida-python \
+		&& ${PYTHON} -m unittest tests.test_core tests.test_tracer
+check-python-64: build/frida-linux-x86_64-stripped/lib/$(PYTHON_NAME)/site-packages/frida build/frida-linux-x86_64-stripped/lib/$(PYTHON_NAME)/site-packages/_frida.so ##@bindings Test Python bindings for x86-64
+	export PYTHONPATH="$(shell pwd)/build/frida-linux-x86_64-stripped/lib/$(PYTHON_NAME)/site-packages" \
 		&& cd frida-python \
 		&& ${PYTHON} -m unittest tests.test_core tests.test_tracer
 
@@ -225,10 +224,9 @@ build/frida-%-stripped/lib/node_modules/frida: build/frida-%/lib/pkgconfig/frida
 		&& strip --strip-all ../$@.tmp/lib/binding/Release/node-*/frida_binding.node \
 		&& mv ../$@.tmp ../$@
 
-check-node-32: check-node-i386 ##@bindings Test Node.js bindings for i386
-check-node-64: check-node-x86_64 ##@bindings Test Node.js bindings for x86-64
-
-check-node-%: build/frida-%-stripped/lib/node_modules/frida
+check-node-32: build/frida-linux-i386-stripped/lib/node_modules/frida ##@bindings Test Node.js bindings for i386
+	$(NODE) --expose-gc $</node_modules/mocha/bin/_mocha
+check-node-64: build/frida-linux-x86_64-stripped/lib/node_modules/frida ##@bindings Test Node.js bindings for x86-64
 	$(NODE) --expose-gc $</node_modules/mocha/bin/_mocha
 
 
@@ -236,9 +234,9 @@ check-node-%: build/frida-%-stripped/lib/node_modules/frida
 	help \
 	distclean clean clean-submodules git-submodules git-submodule-stamps \
 	capstone-32 capstone-64 capstone-update-submodule-stamp \
-	gum-32 gum-64 check-gum-32 check-gum-64 check-gum-linux-i386 check-gum-linux-x86_64 frida-gum-update-submodule-stamp \
-	core-32 core-64 check-core-32 check-core-64 check-core-linux-i386 check-core-linux-x86_64 frida-core-update-submodule-stamp \
+	gum-32 gum-64 check-gum-32 check-gum-64 frida-gum-update-submodule-stamp \
+	core-32 core-64 check-core-32 check-core-64 frida-core-update-submodule-stamp \
 	android-server-i386 android-server-arm \
-	python-32 python-64 check-python-32 check-python-64 check-python-i386 check-python-x86_64 frida-python-update-submodule-stamp \
-	node-32 node-64 check-node-32 check-node-64 check-node-i386 check-node-x86_64 frida-node-update-submodule-stamp
+	python-32 python-64 check-python-32 check-python-64 frida-python-update-submodule-stamp \
+	node-32 node-64 check-node-32 check-node-64 frida-node-update-submodule-stamp
 .SECONDARY:
