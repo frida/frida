b2e181d8e85e8635187ad4883b8a77b90b329e29
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2015-04-02 12:10:28 +0200

    Update build system to use the new Vala compiler

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -317,7 +317,7 @@ if [ "$FRIDA_ENV_SDK" != 'none' ]; then
   PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$FRIDA_SDKROOT/lib/pkgconfig"
 fi
 
-VALAC="$FRIDA_TOOLROOT/bin/valac-0.26 --vapidir=\"$FRIDA_TOOLROOT/share/vala-0.26/vapi\""
+VALAC="$FRIDA_TOOLROOT/bin/valac-0.28 --vapidir=\"$FRIDA_TOOLROOT/share/vala-0.28/vapi\""
 if [ "$FRIDA_ENV_SDK" != 'none' ]; then
   VALAC="$VALAC --vapidir=\"$FRIDA_SDKROOT/share/vala/vapi\""
 fi
