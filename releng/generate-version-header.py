1e8565a23226df48efaaa57df935ce5c935c74d2
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2015-04-17 23:50:34 +0200

    Fix the version header build system integration on Windows

--- releng/generate-version-header.py
+++ releng/generate-version-header.py
@@ -2,14 +2,16 @@
 
 import os
 import subprocess
+import sys
 
-build_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-version = subprocess.check_output(["git", "describe", "--tags", "--always", "--long"], cwd=build_dir).strip().replace("-", ".")
-(major, minor, micro, nano, commit) = version.split(".")
-if nano == "0":
-    version = ".".join([major, minor, micro])
+def generate_version_header():
+    build_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    version = subprocess.check_output(["git", "describe", "--tags", "--always", "--long"], cwd=build_dir).strip().replace("-", ".")
+    (major, minor, micro, nano, commit) = version.split(".")
+    if nano == "0":
+        version = ".".join([major, minor, micro])
 
-print("""\
+    header = """\
 #ifndef __FRIDA_VERSION_H__
 #define __FRIDA_VERSION_H__
 
@@ -20,4 +22,23 @@ print("""\
 #define FRIDA_MICRO_VERSION {micro}
 #define FRIDA_NANO_VERSION {nano}
 
-#endif""").format(version=version, major=major, minor=minor, micro=micro, nano=nano)
+#endif\n""".format(version=version, major=major, minor=minor, micro=micro, nano=nano)
+
+    if len(sys.argv) == 1:
+        sys.stdout.write(header)
+        sys.stdout.flush()
+    else:
+        output_filename = sys.argv[1]
+        try:
+            with open(output_filename, "rb") as f:
+                existing_header = f.read()
+                if header == existing_header:
+                    return
+        except:
+            pass
+        with open(output_filename, "wb") as f:
+            f.write(header)
+
+
+if __name__ == '__main__':
+    generate_version_header()
