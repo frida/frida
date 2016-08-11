e7d04d1a29eece3a28d93a559eea0567cf29adf7
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-08-11 15:32:53 +0200

    Update the V8 version

--- Makefile.sdk.mk
+++ Makefile.sdk.mk
@@ -465,7 +465,7 @@ build/fs-%/lib/pkgconfig/v8.pc: build/fs-tmp-%/.v8-build-stamp
 	echo "" >> $@.tmp
 	echo "Name: V8" >> $@.tmp
 	echo "Description: V8 JavaScript Engine" >> $@.tmp
-	echo "Version: 5.4.0.0" >> $@.tmp
+	echo "Version: 5.4.401.0" >> $@.tmp
 	echo "Libs: -L\$${libdir} -lv8_base -lv8_snapshot -lv8_libplatform -lv8_libbase$(v8_libs_private)" >> $@.tmp
 	echo "Cflags: -I\$${includedir} -I\$${includedir}/include" >> $@.tmp
 	mv $@.tmp $@
