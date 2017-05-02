8ddebb996acc3b695e825ced736e95c6b5e9f655
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-05-03 01:40:52 +0200

    Bump SDKs and toolchains to 20170503

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -134,8 +134,8 @@ fi
 
 prompt_color=33
 
-toolchain_version=20170502
-sdk_version=20170502
+toolchain_version=20170503
+sdk_version=20170503
 if [ $enable_asan = yes ]; then
   sdk_version="$sdk_version-asan"
 fi
