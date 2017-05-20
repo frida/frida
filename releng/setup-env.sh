c54db557554b1ed37350836143d139cafeee06b3
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-05-20 04:07:49 +0200

    Bump toolchain and SDK versions
    No changes this time, just making sure the cache invalidation works.

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -108,8 +108,8 @@ fi
 
 prompt_color=33
 
-toolchain_version=20170503
-sdk_version=20170503
+toolchain_version=20170520
+sdk_version=20170520
 if [ $enable_asan = yes ]; then
   sdk_version="$sdk_version-asan"
 fi
