a543e24166eecebfcd4a54c89f17195e8c37a4ca
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-07-28 16:54:56 +0200

    Add a few missing symbol mappings

--- releng/devkit.py
+++ releng/devkit.py
@@ -290,7 +290,7 @@ def generate_library_unix(package, frida_root, host, output_dir, library_filenam
     return (extra_flags, thirdparty_symbol_mappings)
 
 def extract_public_thirdparty_symbol_mappings(mappings):
-    public_prefixes = ['g_', 'gee_', 'json_']
+    public_prefixes = ["g_", "glib_", "gobject_", "gio_", "gee_", "json_"]
     return [(original, renamed) for original, renamed in mappings if any([original.startswith(prefix) for prefix in public_prefixes])]
 
 def get_thirdparty_symbol_mappings(library, rc):
