998a8904c0f35859fd9897d715f26a0bc899c1ae
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-09-24 20:10:47 +0000

    Bump automake to 1.15.1

--- Makefile.toolchain.mk
+++ Makefile.toolchain.mk
@@ -6,7 +6,7 @@ repo_suffix = ".git"
 
 m4_version := 1.4.18
 autoconf_version := 2.69
-automake_version := 1.15
+automake_version := 1.15.1
 libtool_version := 2.4.6
 gettext_version := 0.19.8.1
 pkg_config_version := 0.29.2
