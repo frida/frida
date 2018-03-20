6d24d0d3918573464951bba8584c2a0ac4666df9
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-03-20 16:51:25 +0100

    Bump Electron prebuilds to 1.8.x and 2.0.0-beta.4

--- releng/release.py
+++ releng/release.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
                 do([npm, "publish"])
             do_build_command([npm, "install"])
             do_build_command([npm, "run", "prebuild", "--", "-t", "8.0.0", "-t", "9.0.0"])
-            do_build_command([npm, "run", "prebuild", "--", "-t", "1.7.9", "-t", "1.8.2-beta.1", "-r", "electron"])
+            do_build_command([npm, "run", "prebuild", "--", "-t", "1.8.4", "-t", "2.0.0-beta.4", "-r", "electron"])
             packages = glob.glob(os.path.join(frida_node_dir, "prebuilds", "*.tar.gz"))
             for package in packages:
                 with open(package, 'rb') as package_file:
