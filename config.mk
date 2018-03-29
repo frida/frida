ee78e575b1fbf2a6b3d3f3748b9e193fa0b3ab63
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-03-29 23:01:49 +0200

    Allow overriding which Python 3.x to use for Meson

--- config.mk
+++ config.mk
@@ -16,11 +16,13 @@ PYTHON ?= $(shell which python)
 PYTHON_VERSION := $(shell $(PYTHON) -c 'import sys; v = sys.version_info; print("{0}.{1}".format(v[0], v[1]))')
 PYTHON_NAME ?= python$(PYTHON_VERSION)
 
+PYTHON3 ?= python3
+
 NODE ?= $(shell which node)
 NODE_BIN_DIR := $(shell dirname $(NODE) 2>/dev/null)
 NPM ?= $(NODE_BIN_DIR)/npm
 
-MESON ?= python3 $(FRIDA)/releng/meson/meson.py
+MESON ?= $(PYTHON3) $(FRIDA)/releng/meson/meson.py
 NINJA ?= $(FRIDA)/releng/ninja-$(build_platform_arch)
 
 tests ?=
