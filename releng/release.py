8dac79e61e3c170f94285c183b813ea478eab6d3
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-07-17 20:58:47 +0200

    Also release prebuilds for Electron 3.0.0-beta.3

--- releng/release.py
+++ releng/release.py
@@ -173,7 +173,7 @@ if __name__ == '__main__':
             if publish:
                 do([npm, "publish"])
             do_build_command([npm, "run", "prebuild", "--", "-t", "8.0.0", "-t", "9.0.0", "-t", "10.0.0"])
-            do_build_command([npm, "run", "prebuild", "--", "-t", "2.0.0", "-r", "electron"])
+            do_build_command([npm, "run", "prebuild", "--", "-t", "2.0.0", "-t", "3.0.0-beta.3", "-r", "electron"])
             packages = glob.glob(os.path.join(frida_node_dir, "prebuilds", "*.tar.gz"))
             for package in packages:
                 with open(package, 'rb') as package_file:
