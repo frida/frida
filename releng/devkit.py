018f2efde1fe7cd24d0d9d009ee2fe0a2667c7df
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-08-22 00:10:45 +0200

    Make devkit generation silent once again

--- releng/devkit.py
+++ releng/devkit.py
@@ -215,10 +215,10 @@ def generate_library_windows(package, frida_root, host, output_dir, library_file
 
     input_libs = [package_lib_path] + package_lib_deps
 
-    subprocess.run(
+    subprocess.check_output(
         [msvs_lib_exe(host), "/nologo", "/out:" + os.path.join(output_dir, library_filename)] + input_libs,
         cwd=msvs_runtime_path(host),
-        check=True)
+        shell=False)
 
     extra_flags = [os.path.basename(lib_path) for lib_path in input_libs]
     thirdparty_symbol_mappings = []
