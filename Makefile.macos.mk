f24c843d4e7e608628a26f5c80bfde4f57b54b28
Toby Petrice <tobypetrice@protonmail.com>
2018-10-23 11:27:37 -0400

    Mixed line continuation

--- Makefile.macos.mk
+++ Makefile.macos.mk
@@ -543,7 +543,7 @@ check-tools-macos-thin: tools-macos-thin ##@tools Test CLI tools for macOS witho
 	capstone-update-submodule-stamp \
 	gum-macos gum-macos-thin gum-ios gum-ios-thin gum-android check-gum-macos check-gum-macos-thin frida-gum-update-submodule-stamp \
 	core-macos core-macos-thin core-ios core-ios-thin core-android check-core-macos check-core-macos-thin check-core-android-arm64 frida-core-update-submodule-stamp \
-	server-macos server-macos-thin server-ios server-ios-thin server-android server-android-arm
+	server-macos server-macos-thin server-ios server-ios-thin server-android server-android-arm \
 	gadget-macos gadget-macos-thin gadget-ios gadget-ios-thin gadget-android \
 	python-macos python-macos-thin check-python-macos check-python-macos-thin frida-python-update-submodule-stamp \
 	node-macos node-macos-thin check-node-macos check-node-macos-thin frida-node-update-submodule-stamp \
