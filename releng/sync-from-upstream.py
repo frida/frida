c9dd7624f540a3b2169ad88c122f1831d4eb2c0d
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-04-01 00:30:18 +0200

    Improve the upstream sync script
    Now supports resuming after a failed cherry-pick.

--- releng/sync-from-upstream.py
+++ releng/sync-from-upstream.py
@@ -27,47 +27,68 @@ upstreams = {
 def sync(repo_path):
     repo_name = os.path.basename(repo_path)
 
-    upstream_url = upstreams.get(repo_name, None)
-    if upstream_url is None:
-        raise UnknownUpstreamError("Unknown upstream: {}".format(repo_name))
+    patches_path = os.path.join(repo_path, ".git", "frida-patches")
+    if os.path.exists(patches_path):
+        patches = PendingPatches.load(patches_path)
 
-    print("Synchronizing with {}".format(upstream_url))
+        print("Applying {} pending patches".format(patches.count))
+    else:
+        upstream_url = upstreams.get(repo_name, None)
+        if upstream_url is None:
+            raise UnknownUpstreamError("Unknown upstream: {}".format(repo_name))
 
-    subprocess.run(["git", "checkout", "master"], cwd=repo_path, capture_output=True, check=True)
-    subprocess.run(["git", "pull"], cwd=repo_path, capture_output=True, check=True)
-    result = subprocess.run(["git", "status"], cwd=repo_path, capture_output=True, check=True, encoding='utf-8')
-    if not "working tree clean" in result.stdout:
-        raise WorkingTreeDirtyError("Working tree is dirty")
+        print("Synchronizing with {}".format(upstream_url))
 
-    subprocess.run(["git", "remote", "add", "upstream", upstream_url], cwd=repo_path, capture_output=True)
-    subprocess.run(["git", "fetch", "upstream"], cwd=repo_path, check=True)
+        subprocess.run(["git", "checkout", "master"], cwd=repo_path, capture_output=True, check=True)
+        subprocess.run(["git", "pull"], cwd=repo_path, capture_output=True, check=True)
+        result = subprocess.run(["git", "status"], cwd=repo_path, capture_output=True, check=True, encoding='utf-8')
+        if not "working tree clean" in result.stdout:
+            raise WorkingTreeDirtyError("Working tree is dirty")
 
-    patches, base = list_our_patches(repo_path)
-    print("We have {} patches on top of upstream".format(len(patches)))
+        subprocess.run(["git", "remote", "add", "upstream", upstream_url], cwd=repo_path, capture_output=True)
+        subprocess.run(["git", "fetch", "upstream"], cwd=repo_path, check=True)
 
-    new_entries = list_upstream_changes(repo_path, base)
-    if len(new_entries) == 0:
-        print("Already up-to-date")
-        return
+        patches, base = list_our_patches(repo_path)
+        print("We have {} patches on top of upstream".format(patches.count))
 
-    print("Upstream has {} new commits".format(len(new_entries)))
+        new_entries = list_upstream_changes(repo_path, base)
+        if len(new_entries) == 0:
+            print("Already up-to-date")
+            return
 
-    print("Merging...")
-    subprocess.run(["git", "merge", "-s", "ours", "upstream/master"], cwd=repo_path, capture_output=True, check=True)
-    subprocess.run(["git", "checkout", "--detach", "upstream/master"], cwd=repo_path, capture_output=True, check=True)
-    subprocess.run(["git", "reset", "--soft", "master"], cwd=repo_path, capture_output=True, check=True)
-    subprocess.run(["git", "checkout", "master"], cwd=repo_path, capture_output=True, check=True)
-    subprocess.run(["git", "commit", "--amend", "-C", "HEAD"], cwd=repo_path, capture_output=True, check=True)
+        print("Upstream has {} new commits".format(len(new_entries)))
 
-    for index, entry in enumerate(patches):
-        cid, message = entry
-        print("Cherry-picking {}/{}: {}".format(index + 1, len(patches), message))
-        subprocess.run(["git", "cherry-pick", cid], cwd=repo_path, capture_output=True, check=True)
+        print("Merging...")
+        subprocess.run(["git", "merge", "-s", "ours", "upstream/master"], cwd=repo_path, capture_output=True, check=True)
+        subprocess.run(["git", "checkout", "--detach", "upstream/master"], cwd=repo_path, capture_output=True, check=True)
+        subprocess.run(["git", "reset", "--soft", "master"], cwd=repo_path, capture_output=True, check=True)
+        subprocess.run(["git", "checkout", "master"], cwd=repo_path, capture_output=True, check=True)
+        subprocess.run(["git", "commit", "--amend", "-C", "HEAD"], cwd=repo_path, capture_output=True, check=True)
 
+        patches.save(patches_path)
+
+    while True:
+        index, cid, message = patches.try_pop()
+        if index is None:
+            break
+
+        print("Cherry-picking {}/{}: {}".format(index + 1, patches.count, message))
+        try:
+            subprocess.run(["git", "cherry-pick", cid], cwd=repo_path, capture_output=True, encoding='utf-8', check=True)
+        except subprocess.CalledProcessError as e:
+            patches.save(patches_path)
+
+            print("\n*** Unable to apply this patch:")
+            print(e.stderr)
+            print("Run `git cherry-pick --abort` and re-run script to skip it.")
+
+            return
+
+    os.remove(patches_path)
     print("Done!")
 
 def list_our_patches(repo_path):
-    patches = []
+    items = []
     base = None
     entries = list_recent_commits(repo_path, "--max-count=1000")
     for index, entry in enumerate(entries):
@@ -75,22 +96,66 @@ def list_our_patches(repo_path):
         if message.startswith("Merge"):
             base = entries[index + 1][0]
             break
-        patches.append((cid, message))
-    patches.reverse()
-    return (patches, base)
+        items.append(("pending", cid, message))
+    items.reverse()
+    return (PendingPatches(items), base)
 
 def list_upstream_changes(repo_path, since):
     return list(reversed(list_recent_commits(repo_path, since + "..upstream/master")))[:-1]
 
 def list_recent_commits(repo_path, *args):
     result = subprocess.run(["git", "log", "--pretty=oneline", "--abbrev-commit", "--topo-order"] + list(args),
-        cwd=repo_path, capture_output=True, check=True, encoding='utf-8')
+        cwd=repo_path, capture_output=True, check=True, encoding='utf-8', errors='surrogateescape')
     return [line.split(" ", 1) for line in result.stdout.rstrip().split("\n")]
 
+
+class PendingPatches(object):
+    def __init__(self, items):
+        self._items = items
+
+        offset = 0
+        for status, cid, message in items:
+            if status == "applied":
+                offset += 1
+            else:
+                break
+        self._offset = offset
+
+    @property
+    def count(self):
+        return len(self._items)
+
+    def try_pop(self):
+        index = self._offset
+        if index == len(self._items):
+            return (None, None, None)
+
+        _, cid, message = self._items[index]
+        self._items[index] = ("applied", cid, message)
+        self._offset += 1
+
+        return (index, cid, message)
+
+    @classmethod
+    def load(cls, path):
+        with open(path, "r", encoding='utf-8') as f:
+            data = f.read()
+
+        items = []
+        for line in data.strip().split("\n"):
+            status, cid, message = line.split(" ", maxsplit=2)
+            items.append((status, cid, message))
+        return PendingPatches(items)
+
+    def save(self, path):
+        data = "\n".join([" ".join(item) for item in self._items]) + "\n"
+        with open(path, "w", encoding='utf-8') as f:
+            f.write(data)
 
 
 class WorkingTreeDirtyError(Exception):
     pass
+
 
 class UnknownUpstreamError(Exception):
     pass
