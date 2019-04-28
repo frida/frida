5872ca3be34cce7edb2c3be490e46962c91fe52a
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-04-28 17:00:56 +0200

    Fix build from a pristine repo on newer GNU Make
    The submodule stamp logic ended up getting performed too late.

--- Makefile
+++ Makefile
@@ -1,3 +1,33 @@
+modules = capstone frida-gum frida-core frida-python frida-node frida-tools
+
+git-submodules:
+	@if [ ! -f frida-core/meson.build ]; then \
+		git submodule init; \
+		git submodule update; \
+	fi
+-include git-submodules
+
+define make-update-submodule-stamp
+$1-update-submodule-stamp: git-submodules
+	@mkdir -p build
+	@cd $1 \
+		&& git log -1 --format=%H > ../build/.$1-submodule-stamp.tmp \
+		&& git status >> ../build/.$1-submodule-stamp.tmp \
+		&& git diff >> ../build/.$1-submodule-stamp.tmp
+	@if [ -f build/.$1-submodule-stamp ]; then \
+		if cmp -s build/.$1-submodule-stamp build/.$1-submodule-stamp.tmp; then \
+			rm build/.$1-submodule-stamp.tmp; \
+		else \
+			mv build/.$1-submodule-stamp.tmp build/.$1-submodule-stamp; \
+		fi \
+	else \
+		mv build/.$1-submodule-stamp.tmp build/.$1-submodule-stamp; \
+	fi
+endef
+$(foreach m,$(modules),$(eval $(call make-update-submodule-stamp,$m)))
+git-submodule-stamps: $(foreach m,$(modules),$m-update-submodule-stamp)
+-include git-submodule-stamps
+
 all $(MAKECMDGOALS):
 	@build_os=$$(uname -s | tr '[A-Z]' '[a-z]' | sed 's,^darwin$$,macos,'); \
 	$(MAKE) -f Makefile.$$build_os.mk $(MAKECMDGOALS)
--- releng/common.mk
+++ releng/common.mk
@@ -13,37 +13,6 @@ frida_tools := frida frida-discover frida-kill frida-ls-devices frida-ps frida-t
 
 v8_api_version := 7.0
 
-
-modules = capstone frida-gum frida-core frida-python frida-node frida-tools
-
-git-submodules:
-	@if [ ! -f frida-core/meson.build ]; then \
-		git submodule init; \
-		git submodule update; \
-	fi
--include git-submodules
-
-define make-update-submodule-stamp
-$1-update-submodule-stamp: git-submodules
-	@mkdir -p build
-	@cd $1 \
-		&& git log -1 --format=%H > ../build/.$1-submodule-stamp.tmp \
-		&& git status >> ../build/.$1-submodule-stamp.tmp \
-		&& git diff >> ../build/.$1-submodule-stamp.tmp
-	@if [ -f build/.$1-submodule-stamp ]; then \
-		if cmp -s build/.$1-submodule-stamp build/.$1-submodule-stamp.tmp; then \
-			rm build/.$1-submodule-stamp.tmp; \
-		else \
-			mv build/.$1-submodule-stamp.tmp build/.$1-submodule-stamp; \
-		fi \
-	else \
-		mv build/.$1-submodule-stamp.tmp build/.$1-submodule-stamp; \
-	fi
-endef
-$(foreach m,$(modules),$(eval $(call make-update-submodule-stamp,$m)))
-git-submodule-stamps: $(foreach m,$(modules),$m-update-submodule-stamp)
--include git-submodule-stamps
-
 build/frida-env-%.rc: releng/setup-env.sh releng/config.site.in build/frida-version.h
 	FRIDA_HOST=$* \
 		FRIDA_OPTIMIZATION_FLAGS="$(FRIDA_OPTIMIZATION_FLAGS)" \
