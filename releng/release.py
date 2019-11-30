d3c01da41a8057da236a909044314ab1227323c1
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-12-01 00:47:51 +0100

    Publish Node.js binaries for v13

--- releng/release.py
+++ releng/release.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
             do_build_command([npm, "install"])
             if publish:
                 do([npm, "publish"])
-            do_build_command([npm, "run", "prebuild", "--", "-t", "8.0.0", "-t", "10.0.0", "-t", "12.0.0"])
+            do_build_command([npm, "run", "prebuild", "--", "-t", "8.0.0", "-t", "10.0.0", "-t", "12.0.0", "-t", "13.0.0"])
             do_build_command([npm, "run", "prebuild", "--", "-t", "7.0.0", "-r", "electron"])
             packages = glob.glob(os.path.join(frida_node_dir, "prebuilds", "*.tar.gz"))
             for package in packages:
