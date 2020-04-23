15eab0a4d205b88368004b67da2fc8acff3c2176
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-23 15:37:13 +0200

    Bump toolchain and SDK versions to 20200422

--- releng/setup-env.sh
+++ releng/setup-env.sh
@@ -154,8 +154,8 @@ fi
 
 prompt_color=33
 
-toolchain_version=20200407
-sdk_version=20200407
+toolchain_version=20200422
+sdk_version=20200422
 if [ $enable_asan = yes ]; then
   sdk_version="$sdk_version-asan"
 fi
