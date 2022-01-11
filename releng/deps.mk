2756993fa4f0dc23b2d3aa4a40d0e3cb6082ae27
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-01-11 20:42:16 +0100

    deps: Bump XZ Utils to 5eae990

--- releng/deps.mk
+++ releng/deps.mk
@@ -297,7 +297,7 @@ libdwarf_deps_for_build = \
 	$(NULL)
 
 xz_name = XZ Utils
-xz_version = 6c84113065f603803683d30342207c73465bbc12
+xz_version = 5eae99056c1f73497afe2e2d1edada3ec72713d8
 xz_url = $(frida_base_url)/xz.git
 xz_hash = $(NULL)
 xz_recipe = autotools
