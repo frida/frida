187854f9a757bf2918c7ec699800b44fc20dd100
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-01-19 15:59:19 +0100

    deps: Bump V8 to caf5a97

--- releng/deps.mk
+++ releng/deps.mk
@@ -613,7 +613,7 @@ openssl_deps_for_build = \
 	$(NULL)
 
 v8_name = V8
-v8_version = f9cdaaa63abb429ce33c18896c3db78f68c3cb49
+v8_version = caf5a9760bac354cafd87469632cd690d235d256
 v8_url = $(frida_base_url)/v8.git
 v8_hash = $(NULL)
 v8_recipe = custom
