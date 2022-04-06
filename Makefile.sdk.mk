35caace204dfc9560a6d1888e90e0b9179fe1cc8
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-04-06 16:00:34 +0200

    deps: Do not build V8 for QNX by default
    Limited default stack space limits its usefulness.

--- Makefile.sdk.mk
+++ Makefile.sdk.mk
@@ -5,6 +5,10 @@ include releng/deps.mk
 MAKE_J ?= -j 8
 SHELL := $(shell which bash)
 
+ifeq ($(FRIDA_V8), auto)
+FRIDA_V8 := $(shell echo $(host_os_arch) | grep -Evq "^qnx-" && echo "enabled" || echo "disabled")
+endif
+
 
 packages = \
 	zlib \
