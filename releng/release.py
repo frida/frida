ebe52d09bebf02022a6502d90c207829bb5c4493
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-05-06 20:25:35 +0200

    release: Skip Node.js 18 prebuilds for Windows/x86
    As Node.js stopped shipping 32-bit Windows binaries.

--- releng/release.py
+++ releng/release.py
@@ -200,6 +200,9 @@ if __name__ == '__main__':
             if system == 'FreeBSD':
                 node_versions = ["14.0.0", "16.0.0", "17.0.1"]
                 electron_version = "16.0.0"
+            elif system == 'Windows' and "(x86)" in node:
+                node_versions = ["10.0.0", "12.0.0", "14.0.0", "16.0.0"]
+                electron_version = "18.0.0"
             else:
                 node_versions = ["10.0.0", "12.0.0", "14.0.0", "16.0.0", "18.0.0"]
                 electron_version = "18.0.0"
