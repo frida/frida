cb0de2f55be579e0fdbe579db7d27dc9a3bcc605
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-01-25 22:01:58 +0100

    releng: Migrate to separate submodule
    To make it easier to reuse tools in other repos.

--- releng/detect-os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -n "$FRIDA_BUILD_OS" ]; then
-  echo $FRIDA_BUILD_OS
-  exit 0
-fi
-
-echo $(uname -s | tr '[A-Z]' '[a-z]' | sed 's,^darwin$,macos,')
--- releng/modules/frida-gadget-ios/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Filesystem path to the universal gadget dylib for iOS.
- */
-export declare const path: string;
-
-/**
- * The gadget's version number as a string, e.g. `"1.2.3"`.
- */
-export declare const version: string;
